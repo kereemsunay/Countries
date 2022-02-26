7b4b8c9390c36629754f012c7e7d115f34ffce2b
kereemsunay <sunay.kerem@outlook.com>
2022-02-26 10:36:48 +0300

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Kerem on 26.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
